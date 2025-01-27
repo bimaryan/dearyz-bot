1c226952022e1f59669e4e9bd06d780280d054f2
bimaryan <bimaryan046@gmail.com>
2025-01-28 03:17:07 +0700

    dashboard pengguna

--- resources/views/Pengguna/Dashboard/index.blade.php
+++ resources/views/Pengguna/Dashboard/index.blade.php
@@ -1,18 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-</head>
-
-<body>
-    <div style="text-align: center; margin-top: 50px;">
-        <h1>Scan QR Code untuk menghubungkan WhatsApp</h1>
-        <div id="qr-code">
-            <p>Menunggu QR Code...</p>
+@extends('Pengguna.index')
+@section('content')
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 mt-14">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+                <div class="bg-gray-100 rounded-lg shadow-lg p-4">
+                    <div class="flex justify-center" id="qr-code">
+                        <p>Menunggu QR Code...</p>
+                    </div>
+                </div>
+                <div class="bg-gray-100 rounded-lg shadow-lg p-4">
+                    <h3 class="text-lg font-medium">Akun Saya</h3>
+                    <hr class="my-3" />
+                    <div>
+                        <h4>Username <span class="ml-2">:</span> {{ Auth::user()->name }}</h4>
+                        <h4>Email <span class="ml-2">:</span> {{ Auth::user()->email }}</h4>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -24,6 +28,7 @@
 
         fetchQRCode();
     </script>
-</body>
+@endsection
 
-</html>
+/node_modules
+./wwebjs_auth\session
